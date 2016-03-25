3089078dafb85054220021c38fe5d1902bd9369f
Muhammad-Usman17 <muhammadusman1794@gmail.com>
2016-03-26 01:41:56 +0500

    Assignment 1& 2

---
+++ Assignment01/Assignment01/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string sibling = string.Empty;
+            int nos = 0;
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.Write("Please enter the number of siblings: ");
+            sibling = Console.ReadLine();
+
+            while (!(Int32.TryParse(sibling, out nos)&&Int32.Parse(sibling)>1))
+            {
+                Console.Write("Not a valid number, try again.:");
+
+                sibling = Console.ReadLine();
+            }
+
+            String[] data = new string[nos];
+            DateTime[] doblist = new DateTime[nos];
+
+
+
+            for (int i = 0; i < nos; i++)
+            {
+
+                Console.Write("Please enter date of birth of sibling  : ");
+                string input = Console.ReadLine();
+                DateTime dateTime;
+                if (DateTime.TryParse(input, out dateTime))
+                {
+                    data[i] = dateTime.ToString("MM/dd/yyyy").Replace('-', '/'); ;
+                }
+
+                doblist[i] = Convert.ToDateTime(data[i]);
+
+            }
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Array.Sort(doblist);
+
+
+            for (int j = 0; j < nos; j++)
+            {
+                int no = j + 1;
+                DateTime today = DateTime.Now;
+                Console.Write("Age of sibling " + no + "is: ");
+                p.dif_fCal(doblist[j], today);
+            }
+
+
+            for (int K = 0; K < nos - 1; K++)
+            {
+                int no = K + 1;
+                Console.Write("Difference between sibling " + no + " and " + (no + 1) + " is: ");
+                p.dif_fCal(doblist[K], doblist[K + 1]);
+            }
+
+            Console.Read();
+        }
+
+
+
+
+
+
+
+
+
+
+        public void dif_fCal(DateTime first, DateTime second)
+        {
+
+            var totalDays = (second - first).TotalDays;
+            var totalYears = Math.Truncate(totalDays / 365);
+            var totalMonths = Math.Truncate((totalDays % 365) / 30);
+            var remainingDays = Math.Truncate((totalDays % 365) % 30);
+            Console.WriteLine(" {0} years {1} months {2} days", totalYears, totalMonths, remainingDays);
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
---
+++ compiler/compiler/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp;
+
+using System.CodeDom.Compiler;
+
+namespace compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press s to write C# code");
+            Console.WriteLine("Press F5 to compile code");
+            Console.WriteLine("Press ECS to EXIT");
+
+            Console.WriteLine("Enter your option: ");
+           
+            Char CHOICE = (char)Console.Read();
+          
+            if (CHOICE=='s')
+            {
+                Console.Clear();
+                writecode();
+
+            }
+            else
+            {
+                Console.WriteLine(" wrong choice: ");
+                
+            }
+            Console.Read();
+
+
+        }
+
+        public static void writecode()
+        {
+            string input="";
+            Console.WriteLine("Enter your code: ");
+            input = Console.ReadLine();
+            while(Console.ReadKey(true).Key != ConsoleKey.F5)
+            {
+                input = input + Console.ReadLine();
+           
+              
+            }
+            
+            test(input);
+
+
+        }
+        public static void test(string SourceString)
+        {
+
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            CompilerParameters parameters = new CompilerParameters();
+            CompilerResults results = icc.CompileAssemblyFromSource(parameters, SourceString);
+            if (results.Errors.Count > 0)
+            {
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    int line = 1;
+                   line = line + CompErr.Line;
+                    Console.WriteLine(
+                         "Line number " + CompErr.Line +
+                         ", Error Number: " + CompErr.ErrorNumber +
+                         ", '" + CompErr.ErrorText + ";" +
+                         Environment.NewLine + Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Success!");
+
+
+            }
+
+        }
+    }
+}
